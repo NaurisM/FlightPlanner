98ca6722652ead30eda69b3c7c2cd7c502348e2f
NaurisM <nauris.magone@gmail.com>
2021-06-09 12:59:05 +0300

    add Request, Airport, Flight classes

---
+++ FlightPlanner/Models/Airport.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FlightPlanner.Models
+{
+    public class Airport
+    {
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string AirportCode { get; set; }
+    }
+}
