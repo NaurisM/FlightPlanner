58f7a78dd2e295d7d79cf77a9939c81cbd05557f
NaurisM <nauris.magone@gmail.com>
2021-06-11 23:47:34 +0300

    add airport storage

--- FlightPlanner/Controllers/CustomerApiController.cs
+++ FlightPlanner/Controllers/CustomerApiController.cs
@@ -4,13 +4,26 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using FlightPlanner.Models;
 
 namespace FlightPlanner.Controllers
 {
     public class CustomerApiController : ApiController
     {
-        [Route("api/airports/{search}")]
-        public IHttpActionResult GetAirport(string search)
+        [Route("api/airports/"), HttpGet]
+        public IHttpActionResult SearchAirports(string airport)
+        {
+            return Ok();
+        }
+
+        [Route("api/flights/search"), HttpPost]
+        public IHttpActionResult SearchFlights(SearchFlightsRequest flight)
+        {
+            return Ok();
+        }
+
+        [Route("api/flights/{id}"), HttpGet]
+        public IHttpActionResult FindFlightById(int id)
         {
             return Ok();
         }
---
+++ FlightPlanner/Models/AirportStorage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FlightPlanner.Models
+{
+    public class AirportStorage
+    {
+        public static List<Airport> AllAirports = new List<Airport>();
+
+        public static void AddAirport(Airport airport)
+        {
+            AllAirports.Add(airport);
+        }
+
+        public static Airport FindAirport(string airportCode)
+        {
+            var result = AllAirports.FirstOrDefault(ap => ap.AirportCode.Contains(airportCode));
+            return result;
+        }
+    }
+}
