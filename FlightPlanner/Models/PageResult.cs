be92837d07eac87f0638e32350fba1f4162acdc2
NaurisM <nauris.magone@gmail.com>
2021-06-11 14:23:34 +0300

    add Page Result class

---
+++ FlightPlanner/Models/PageResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FlightPlanner.Models
+{
+    public class PageResult
+    {
+        public int Number { get; set; }
+        public int TotalItems { get; set; }
+        public Array Items { get; set; }
+    }
+}
