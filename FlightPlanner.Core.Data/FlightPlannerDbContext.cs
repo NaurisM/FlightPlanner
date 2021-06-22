c3c583b9b8db5bf600fa26f794506aeafc92d9f4
NaurisM <nauris.magone@gmail.com>
2021-06-22 18:40:02 +0300

    add data + nuget

---
+++ FlightPlanner.Core.Data/FlightPlannerDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightPlanner.Core.Data
+{
+    public class FlightPlannerDbContext : DbContext
+    {
+        public FlightPlannerDbContext() : base("flight-planner")
+        {
+
+        }
+
+        public DbSet<Flight> Flights { get; set; }
+        public DbSet<Airport> Airports { get; set; }
+    }
+}
+}
---
+++ FlightPlanner.Core.Services/EntityService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlightPlanner.Core.Models;
+
+namespace FlightPlanner.Core.Services
+{
+    public class EntityService<T> : IEntityService<T> where T : Entity
+    {
+        public IQueryable<T> Query()
+        {
+            return Query();
+        }
+
+        public IEnumerable<T> Get()
+        {
+            return Get();
+        }
+
+        public T GetById(int id)
+        {
+            return GetById(id);
+        }
+
+        public void Create(T entity)
+        {
+            Create(entity);
+        }
+
+        public void Update(T entity)
+        {
+            Update(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            Delete(entity);
+        }
+    }
+}
